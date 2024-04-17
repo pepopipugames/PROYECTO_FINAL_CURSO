71b92370cac73644edced5433205c9c390cb7ee3
DvdPires19 <david.pires1997@gmail.com>
2024-04-17 15:17:16 +0200

    Project structure modified

---
+++ project/assets/php/configuracionBBDD.php
@@ -0,0 +1,6 @@
+<?php
+//En que ordenador esta, que usuario, que password y que base de datos
+define ("SERVIDOR", "localhost");
+define ("USUARIO", "root");
+define ("PASSWORD", "");
+define ("BBDD", "bd_academia");
