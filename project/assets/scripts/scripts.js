b92ba37b082e81713e58312a42e7c8af6bf0775f
DvdPires19 <david.pires1997@gmail.com>
2024-04-28 10:20:31 +0200

    function fMostrarFormularios created

--- project/assets/scripts/scripts.js
+++ project/assets/scripts/scripts.js
@@ -1,3 +1,17 @@
+function fMostrarFormularios(nombre_formulario_con_almohadilla) {
+    //Guardamos todos los formularios en una lista de formularios con el querySelectorAll
+    let lista_formularios = document.querySelectorAll("#div_formularios div");
+    //Ocultamos primero todos los formularios internos del div_formularios
+    lista_formularios.forEach(item => {
+        item.style.display = 'none';
+    });
+    //Mostramos el formulario quq queramos dentro de la modal
+    document.querySelector(nombre_formulario_con_almohadilla).style.display = "flex";
+    // Mostramos la modal
+    document.querySelector("#div_formularios").style.display = "flex";
+
+}
+
 function fMostrarMenu() {
 
 }
