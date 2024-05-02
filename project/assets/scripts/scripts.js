9b06d2b4b650af8935128d25789927fb89b6de00
alexsugimoto <alexsugimoto@outlook.com>
2024-05-02 11:04:21 +0200

    Script, estilos e index actualizados

--- project/assets/scripts/scripts.js
+++ project/assets/scripts/scripts.js
@@ -1,6 +1,6 @@
 // VARIABLES GLOBALES
 
-let id_usuario_logueado = ""; // ID DEL USUARIO CUANDO SE REGISTRA
+let id_usuario_logueado = 0; // ID DEL USUARIO CUANDO SE REGISTRA
 
 function fMostrarFormularios(nombre_formulario_con_almohadilla) {
     //Guardamos todos los formularios en una lista de formularios con el querySelectorAll
@@ -29,16 +29,9 @@ function fMostrarModal(nombre_modal_con_almohadilla) {
     lista_modales.forEach(item => {
         item.style.display = "none";
         console.log(item)
-        
-        
     });
 
-    console.log("HOLA")
-
     document.querySelector(nombre_modal_con_almohadilla).style.display = "flex";
-
-    
-
 }
 
 
@@ -70,8 +63,40 @@ function fMostrarActividades(){
 // Muestra la modal principal de preparar viaje (donde eliges la ciudad primero)
 
 function fMostrarPrepararViaje(){
-    
-}
+    // Declaramos 2 array list para el finally
+    let lista_ciudades = [];
+    let lista_fotos = [];
+    // Hacemos la peticion de 6 ciudades aleatorias
+    let sql = "SELECT ciu_nombre, ciu_foto FROM ciudades ORDER BY rand() LIMIT 6;";
+    let URL = "assets/php/servidor.php?peticion=EjecutarSelect";
+    URL += "&sql=" + sql;
+    fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+        let html = "";
+        console.log("Ciudades: ", data)
+        data.datos.forEach(item => {
+            // Reemplazamos los espacios en el nombre de la ciudad por guiones bajos, los ids no aceptan espacios
+            let ciudadId = item.ciu_nombre.replace(/\s+/g, '_');
+            lista_ciudades.push(ciudadId);
+            lista_fotos.push(item.ciu_foto);
+            html += "<div class='ciudades'>"
+            html += `    <div id='${ciudadId}'>`
+            html += `       ${item.ciu_nombre}`
+            html += "    </div>"
+            html += "</div>"
+        });
+        // Las imprimimos dentro de #pv_ciudades
+        document.querySelector("#pv_ciudades").innerHTML = html;
+        fMostrarModal("#modal_preparar_viaje");
+    })
+    .finally(() => {
+        lista_ciudades.forEach((item, indice) => {
+            document.querySelector(`#${item}`).style.backgroundImage = `url(../project/assets/imagenes/ciudades/${lista_fotos[indice]})`;
+            document.querySelector(`#${item}`).style.backgroundSize = "cover";
+        });
+    });
+} 
 // Muestra la Home, pulsando el logo que está en el header
 
 function fMostrarHome(){
