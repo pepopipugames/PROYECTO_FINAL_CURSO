727f1906ac33dc849cd466d206aee36e66e42f1f
alexsugimoto <alexsugimoto@outlook.com>
2024-05-02 10:19:40 +0200

    funcion preparar viaje creada y terminada. Base de datos actualizada con datos de paises y ciudades

--- bbdd/bd_viajes.sql
+++ bbdd/bd_viajes.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 19-04-2024 a las 09:39:17
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.2
+-- Host: localhost
+-- Generation Time: May 02, 2024 at 09:06 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,45 +18,43 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `bd_viajes`
+-- Database: `tuimak`
 --
 
 DELIMITER $$
 --
--- Procedimientos
+-- Procedures
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ciudades_select_all` ()  SELECT * FROM ciudades$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ciudades_select_all` ()   SELECT * FROM ciudades$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ciudades_select_by_id` (IN `_id` INT)  SELECT * FROM ciudades WHERE ciu_id = _id$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ciudades_select_by_id` (IN `_id` INT)   SELECT * FROM ciudades WHERE ciu_id = _id$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `paises_delete` (IN `_id` INT)  DELETE FROM paises WHERE pais_id = _id$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `paises_delete` (IN `_id` INT)   DELETE FROM paises WHERE pais_id = _id$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `paises_insert` (IN `_nombre` VARCHAR(50))  INSERT INTO paises VALUES
+CREATE DEFINER=`root`@`localhost` PROCEDURE `paises_insert` (IN `_nombre` VARCHAR(50))   INSERT INTO paises VALUES
 (null, _nombre)$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `paises_select_all` ()  SELECT * FROM paises ORDER by pais_nombre$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `paises_select_all` ()   SELECT * FROM paises ORDER by pais_nombre$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `paises_select_by_id` (IN `_id` INT)  SELECT * 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `paises_select_by_id` (IN `_id` INT)   SELECT * 
 FROM 
 	paises 
 WHERE 
 	pais_id =_id$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `paises_update` (IN `_id` INT, IN `_nombre` VARCHAR(50))  UPDATE paises SET
+CREATE DEFINER=`root`@`localhost` PROCEDURE `paises_update` (IN `_id` INT, IN `_nombre` VARCHAR(50))   UPDATE paises SET
 	pais_nombre = _nombre
 WHERE
 	pais_id = _id$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `usuarios_insert` (IN `_nombre` VARCHAR(50), IN `_apellido` VARCHAR(50), IN `_alias` VARCHAR(50), IN `_password` VARCHAR(50), IN `_documento` VARCHAR(50), IN `_telefono` VARCHAR(50), IN `_fecha` DATE)  INSERT INTO usuarios VALUES
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usuarios_insert` (IN `_nombre` VARCHAR(50), IN `_apellido` VARCHAR(50), IN `_alias` VARCHAR(50), IN `_password` VARCHAR(50), IN `_documento` VARCHAR(50), IN `_telefono` VARCHAR(50), IN `_fecha` DATE)   INSERT INTO usuarios VALUES
 (null, _nombre, _apellido, _alias, md5(_password), _documento, _telefono, _fecha )$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `usuarios_select_all` ()  SELECT * FROM usuarios ORDER BY usu_nombre$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usuarios_select_all` ()   SELECT * FROM usuarios ORDER BY usu_nombre$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `usuarios_select_by_id` (IN `_id` INT)  NO SQL
-    COMMENT 'We can use this function to select a user by his/her id '
-SELECT * FROM usuarios WHERE usu_id = _id$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usuarios_select_by_id` (IN `_id` INT)  NO SQL COMMENT 'We can use this function to select a user by his/her id ' SELECT * FROM usuarios WHERE usu_id = _id$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `usuarios_update` (IN `_nombre` VARCHAR(50), IN `_apellido` VARCHAR(50), IN `_alias` VARCHAR(50), IN `_password` VARCHAR(50), IN `_documento` VARCHAR(50), IN `_telefono` VARCHAR(50), IN `_fecha` DATE, IN `_id` INT)  UPDATE usuarios SET 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usuarios_update` (IN `_nombre` VARCHAR(50), IN `_apellido` VARCHAR(50), IN `_alias` VARCHAR(50), IN `_password` VARCHAR(50), IN `_documento` VARCHAR(50), IN `_telefono` VARCHAR(50), IN `_fecha` DATE, IN `_id` INT)   UPDATE usuarios SET 
 	usu_nombre = _nombre,
     usu_apellido = _apellido,
     usu_alias = _alias,
@@ -73,7 +70,7 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `actividades`
+-- Table structure for table `actividades`
 --
 
 CREATE TABLE `actividades` (
@@ -83,12 +80,12 @@ CREATE TABLE `actividades` (
   `act_inicio` time NOT NULL,
   `act_duracion` int(11) NOT NULL,
   `act_precio` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `ciudades`
+-- Table structure for table `ciudades`
 --
 
 CREATE TABLE `ciudades` (
@@ -97,31 +94,50 @@ CREATE TABLE `ciudades` (
   `ciu_codigo` varchar(3) NOT NULL,
   `ciu_foto` varchar(50) NOT NULL,
   `ciu_pais_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `ciudades`
+-- Dumping data for table `ciudades`
 --
 
 INSERT INTO `ciudades` (`ciu_id`, `ciu_nombre`, `ciu_codigo`, `ciu_foto`, `ciu_pais_id`) VALUES
-(1, 'madrid', 'MAD', 'madrid.jpg', 1),
-(2, 'paris', 'PAR', 'paris.jpg', 2);
+(1, 'Madrid', 'MAD', 'madrid.jpg', 1),
+(2, 'Paris', 'CDG', 'paris.jpg', 2),
+(4, 'Santo Domingo', 'SDQ', 'santo_domingo.png', 3),
+(5, 'Moscú', 'MOW', 'moscu.png', 4),
+(6, 'Yakarta', 'JKT', 'jakarta.png', 5),
+(7, 'Sofía', 'SOF', 'sofia.png', 6),
+(8, 'Pekín', 'PEK', 'pekin.png', 7),
+(9, 'NDjamena', 'NDJ', 'ndjamena.png', 8),
+(10, 'Praga', 'PRG', 'praga.png', 9),
+(11, 'Varsovia', 'WAW', 'varsovia.png', 10),
+(12, 'Sídney', 'SYD', 'sydney.png', 11),
+(13, 'El Cairo', 'CAI', 'el_cairo.png', 12),
+(14, 'Lima', 'LIM', 'lima.png', 13),
+(15, 'Berlín', 'BER', 'berlin.png', 14),
+(16, 'Zagreb', 'ZAG', 'zagreb.png', 15),
+(17, 'Atenas', 'ATH', 'atenas.png', 16),
+(18, 'Nueva York', 'NYC', 'nueva_york.png', 17),
+(19, 'Kabul', 'KBL', 'kabul.png', 18),
+(20, 'Dodoma', 'DOD', 'dodoma.png', 19),
+(21, 'Bogotá', 'BOG', 'bogota.png', 20),
+(22, 'Buenos Aires', 'BUE', 'buenos_aires.png', 21);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `ciudades_actividades`
+-- Table structure for table `ciudades_actividades`
 --
 
 CREATE TABLE `ciudades_actividades` (
   `ca_act_id` int(11) NOT NULL,
   `ca_ciu_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `historial_user`
+-- Table structure for table `historial_user`
 --
 
 CREATE TABLE `historial_user` (
@@ -142,12 +158,12 @@ CREATE TABLE `historial_user` (
   `hu_fnac_antiguo` date DEFAULT NULL,
   `hu_fnac_nuevo` date NOT NULL,
   `hu_accion` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `hoteles`
+-- Table structure for table `hoteles`
 --
 
 CREATE TABLE `hoteles` (
@@ -156,31 +172,94 @@ CREATE TABLE `hoteles` (
   `hot_logo` varchar(50) NOT NULL,
   `hot_precio_noche` decimal(10,2) NOT NULL,
   `hot_ciu_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `paises`
+-- Table structure for table `paises`
 --
 
 CREATE TABLE `paises` (
   `pais_id` int(11) NOT NULL,
   `pais_nombre` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `paises`
+-- Dumping data for table `paises`
 --
 
 INSERT INTO `paises` (`pais_id`, `pais_nombre`) VALUES
-(1, 'españa'),
-(2, 'francia');
+(1, 'España'),
+(2, 'Francia'),
+(3, 'República Dominicana'),
+(4, 'Rusia'),
+(5, 'Indonesia'),
+(6, 'Bulgaria'),
+(7, 'China'),
+(8, 'Chad'),
+(9, 'República Checa'),
+(10, 'Polonia'),
+(11, 'Australia'),
+(12, 'Egipto'),
+(13, 'Perú'),
+(14, 'Alemania'),
+(15, 'Croacia'),
+(16, 'Grecia'),
+(17, 'Estados Unidos'),
+(18, 'Afganistán'),
+(19, 'Tanzania'),
+(20, 'Colombia'),
+(21, 'Argentina'),
+(23, 'Serbia'),
+(24, 'Territorio Palestino'),
+(25, 'Túnez'),
+(26, 'Filipinas'),
+(27, 'Ucrania'),
+(28, 'Eslovenia'),
+(29, 'Micronesia'),
+(30, 'Francia'),
+(31, 'Kirguistán'),
+(32, 'México'),
+(33, 'Irlanda'),
+(34, 'Noruega'),
+(35, 'Bangladés'),
+(36, 'Somalia'),
+(37, 'Vietnam'),
+(38, 'Bielorrusia'),
+(39, 'Suecia'),
+(40, 'Kenia'),
+(41, 'Brasil'),
+(42, 'España'),
+(43, 'Francia'),
+(44, 'República Dominicana'),
+(45, 'Rusia'),
+(46, 'Indonesia'),
+(47, 'Bulgaria'),
+(48, 'China'),
+(49, 'Chad'),
+(50, 'República Checa'),
+(51, 'Polonia'),
+(52, 'Australia'),
+(53, 'Egipto'),
+(54, 'Perú'),
+(55, 'Alemania'),
+(56, 'Croacia'),
+(57, 'Grecia'),
+(58, 'Estados Unidos'),
+(59, 'Afganistán'),
+(60, 'Tanzania'),
+(61, 'Colombia'),
+(62, 'Argentina'),
+(63, 'Indonesia'),
+(64, 'Serbia'),
+(65, 'Territorio Palestino'),
+(66, 'Túnez');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `reservas_hotel_viajes`
+-- Table structure for table `reservas_hotel_viajes`
 --
 
 CREATE TABLE `reservas_hotel_viajes` (
@@ -191,24 +270,24 @@ CREATE TABLE `reservas_hotel_viajes` (
   `rhv_fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `rhv_fecha_inicio` date NOT NULL,
   `rhv_fecha_fin` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `reserva_act`
+-- Table structure for table `reserva_act`
 --
 
 CREATE TABLE `reserva_act` (
   `ra_act_id` int(11) NOT NULL,
   `ra_usu_id` int(11) NOT NULL,
   `ra_fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Table structure for table `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -220,10 +299,10 @@ CREATE TABLE `usuarios` (
   `usu_documento_identificacion` varchar(50) NOT NULL,
   `usu_telefono` varchar(50) NOT NULL,
   `usu_fnac` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Disparadores `usuarios`
+-- Triggers `usuarios`
 --
 DELIMITER $$
 CREATE TRIGGER `AfterInsert` AFTER INSERT ON `usuarios` FOR EACH ROW INSERT INTO historial_user VALUES
@@ -251,7 +330,7 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `viajes`
+-- Table structure for table `viajes`
 --
 
 CREATE TABLE `viajes` (
@@ -262,20 +341,20 @@ CREATE TABLE `viajes` (
   `viaje_precio` decimal(10,2) NOT NULL,
   `viaje_ida` datetime NOT NULL,
   `viaje_vuelta` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `actividades`
+-- Indexes for table `actividades`
 --
 ALTER TABLE `actividades`
   ADD PRIMARY KEY (`act_id`);
 
 --
--- Indices de la tabla `ciudades`
+-- Indexes for table `ciudades`
 --
 ALTER TABLE `ciudades`
   ADD PRIMARY KEY (`ciu_id`),
@@ -283,34 +362,34 @@ ALTER TABLE `ciudades`
   ADD KEY `r-ciudad-pais` (`ciu_pais_id`);
 
 --
--- Indices de la tabla `ciudades_actividades`
+-- Indexes for table `ciudades_actividades`
 --
 ALTER TABLE `ciudades_actividades`
   ADD PRIMARY KEY (`ca_act_id`,`ca_ciu_id`),
   ADD KEY `r-ca-ciudades` (`ca_ciu_id`);
 
 --
--- Indices de la tabla `historial_user`
+-- Indexes for table `historial_user`
 --
 ALTER TABLE `historial_user`
   ADD PRIMARY KEY (`hu_id`),
   ADD KEY `r-hu-users` (`hu_usu_id`);
 
 --
--- Indices de la tabla `hoteles`
+-- Indexes for table `hoteles`
 --
 ALTER TABLE `hoteles`
   ADD PRIMARY KEY (`hot_id`),
   ADD KEY `r-hoteles-ciudades` (`hot_ciu_id`);
 
 --
--- Indices de la tabla `paises`
+-- Indexes for table `paises`
 --
 ALTER TABLE `paises`
   ADD PRIMARY KEY (`pais_id`);
 
 --
--- Indices de la tabla `reservas_hotel_viajes`
+-- Indexes for table `reservas_hotel_viajes`
 --
 ALTER TABLE `reservas_hotel_viajes`
   ADD PRIMARY KEY (`rhv_id`),
@@ -319,20 +398,20 @@ ALTER TABLE `reservas_hotel_viajes`
   ADD KEY `r-rhv-viajes` (`rhv_viaje_id`);
 
 --
--- Indices de la tabla `reserva_act`
+-- Indexes for table `reserva_act`
 --
 ALTER TABLE `reserva_act`
   ADD PRIMARY KEY (`ra_act_id`,`ra_usu_id`),
   ADD KEY `r-ra-usuarios` (`ra_usu_id`);
 
 --
--- Indices de la tabla `usuarios`
+-- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`usu_id`);
 
 --
--- Indices de la tabla `viajes`
+-- Indexes for table `viajes`
 --
 ALTER TABLE `viajes`
   ADD PRIMARY KEY (`viaje_id`),
@@ -340,88 +419,88 @@ ALTER TABLE `viajes`
   ADD KEY `r-ciudad-viaje-destino` (`viaje_ciu_destino`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `actividades`
+-- AUTO_INCREMENT for table `actividades`
 --
 ALTER TABLE `actividades`
   MODIFY `act_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `ciudades`
+-- AUTO_INCREMENT for table `ciudades`
 --
 ALTER TABLE `ciudades`
-  MODIFY `ciu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ciu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
--- AUTO_INCREMENT de la tabla `historial_user`
+-- AUTO_INCREMENT for table `historial_user`
 --
 ALTER TABLE `historial_user`
   MODIFY `hu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de la tabla `hoteles`
+-- AUTO_INCREMENT for table `hoteles`
 --
 ALTER TABLE `hoteles`
   MODIFY `hot_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `paises`
+-- AUTO_INCREMENT for table `paises`
 --
 ALTER TABLE `paises`
-  MODIFY `pais_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `pais_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 --
--- AUTO_INCREMENT de la tabla `reservas_hotel_viajes`
+-- AUTO_INCREMENT for table `reservas_hotel_viajes`
 --
 ALTER TABLE `reservas_hotel_viajes`
   MODIFY `rhv_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `usuarios`
+-- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT de la tabla `viajes`
+-- AUTO_INCREMENT for table `viajes`
 --
 ALTER TABLE `viajes`
   MODIFY `viaje_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `ciudades`
+-- Constraints for table `ciudades`
 --
 ALTER TABLE `ciudades`
   ADD CONSTRAINT `r-pais-ciudad` FOREIGN KEY (`ciu_pais_id`) REFERENCES `paises` (`pais_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `ciudades_actividades`
+-- Constraints for table `ciudades_actividades`
 --
 ALTER TABLE `ciudades_actividades`
   ADD CONSTRAINT `r-ca-actividades` FOREIGN KEY (`ca_act_id`) REFERENCES `actividades` (`act_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   ADD CONSTRAINT `r-ca-ciudades` FOREIGN KEY (`ca_ciu_id`) REFERENCES `ciudades` (`ciu_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `historial_user`
+-- Constraints for table `historial_user`
 --
 ALTER TABLE `historial_user`
   ADD CONSTRAINT `r-hu-users` FOREIGN KEY (`hu_usu_id`) REFERENCES `usuarios` (`usu_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `hoteles`
+-- Constraints for table `hoteles`
 --
 ALTER TABLE `hoteles`
   ADD CONSTRAINT `r-ciudades-hoteles` FOREIGN KEY (`hot_ciu_id`) REFERENCES `ciudades` (`ciu_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `reservas_hotel_viajes`
+-- Constraints for table `reservas_hotel_viajes`
 --
 ALTER TABLE `reservas_hotel_viajes`
   ADD CONSTRAINT `r-rhv-hoteles` FOREIGN KEY (`rhv_hot_id`) REFERENCES `hoteles` (`hot_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
@@ -429,14 +508,14 @@ ALTER TABLE `reservas_hotel_viajes`
   ADD CONSTRAINT `r-rhv-viajes` FOREIGN KEY (`rhv_viaje_id`) REFERENCES `viajes` (`viaje_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `reserva_act`
+-- Constraints for table `reserva_act`
 --
 ALTER TABLE `reserva_act`
   ADD CONSTRAINT `r-ra-actividades` FOREIGN KEY (`ra_act_id`) REFERENCES `actividades` (`act_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   ADD CONSTRAINT `r-ra-usuarios` FOREIGN KEY (`ra_usu_id`) REFERENCES `usuarios` (`usu_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `viajes`
+-- Constraints for table `viajes`
 --
 ALTER TABLE `viajes`
   ADD CONSTRAINT `r-ciudad-viaje-destino` FOREIGN KEY (`viaje_ciu_destino`) REFERENCES `ciudades` (`ciu_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
